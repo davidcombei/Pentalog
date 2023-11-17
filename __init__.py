440b8bcd13f462fa611e4df69a092a092b9b55b1
davidcombei <102519601+davidcombei@users.noreply.github.com>
2023-11-18 00:09:25 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
 """ 
 Acest pachet contine modulele necesare proiectului de la cursul de Python in colaborare cu Pentalog...
 """
-
-
+import tema1
+#luam input de la tastatura adresa URL dorita
+my_url=input("Adresa URL este : ")
+#apelul functiei create in modulul anterior
+tema1.print_html_meta(my_url)
