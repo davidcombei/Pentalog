31edded67cb0ca010e93fb135704b4c897b05a58
davidcombei <102519601+davidcombei@users.noreply.github.com>
2023-12-10 22:44:39 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,7 @@
+"""
+Acest pachet contine modulele necesare proiectului de la cursul de Python in colaborare cu Pentalog
+"""
+import meta
+
+#apelul functiei create in modulul anterior care va citi din config.ini adresa URL si afisa titlul HTML si description meta-ul
+meta.print_html_meta()
