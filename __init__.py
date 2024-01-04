41b9d4689a6be5c9551d3493caba1ebf34f75774
davidcombei <102519601+davidcombei@users.noreply.github.com>
2024-01-04 21:00:51 +0200

    Delete __init__.py

--- __init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Acest pachet contine modulele necesare proiectului de la cursul de Python in colaborare cu Pentalog
-"""
-import meta
-
-#apelul functiei create in modulul anterior care va citi din config.ini adresa URL si afisa titlul HTML si description meta-ul
-meta.print_html_meta()
